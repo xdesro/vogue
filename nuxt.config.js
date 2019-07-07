bff18059cc5328b4bd03651c1086e2f17bc21a6a
Henry Desroches <yo@henry.codes>
2019-07-07 10:12:26 -0600

    📱  - Responsive meta and gitignore .nuxt dir

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,9 @@ export default {
   css: ["~/assets/scss/style.scss"],
   router: {
     linkExactActiveClass: "nav__link--active"
-  }
+  },
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" }
+  ]
 };
