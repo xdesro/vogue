dab681499cae518bea1043e11cf3de1a34e465fd
Henry Desroches <yo@henry.codes>
2019-07-07 10:47:04 -0600

    🗑  - Remove .nuxt from git

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,10 @@ export default {
   router: {
     linkExactActiveClass: "nav__link--active"
   },
-  meta: [
-    { charset: "utf-8" },
-    { name: "viewport", content: "width=device-width, initial-scale=1" }
-  ]
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ]
+  }
 };
