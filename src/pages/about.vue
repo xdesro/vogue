406f7ee4665fbf1ace70ce1de90846279350ce2b
Henry Desroches <yo@henry.codes>
2019-07-14 09:47:47 -0600

    🗣  - About page for large screens, movin' right along.

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -8,12 +8,13 @@
       <figure class="main__img about__profile-pic">
         <img src="~/assets/img/spooky-profile-pic.png" alt="Henry Desroches lookin' spooky." />
       </figure>
+      <img class="about__logo" src="~/assets/img/icon-logo.svg" alt />
       <h2 class="about__subtitle">
         Hey there, I'm
         <br />
         <span class="about__subtitle--light">Henry Desroches.</span>
       </h2>
-      <div>
+      <div class="about__content">
         <p>I’m a creative developer currently based in Denver, Colorado. I’m really into CSS, animation, developer experience, and deleting code.</p>
         <p>I love bending the rules of CSS, making generative art, and WebGL. I have a lot of experience with style code at scale, JS-agnostic design systems, and static site generators. In my non-code time, I foster greyhounds, tweet too much, and ride bikes.</p>
         <p>I’m looking for a new role working on design systems, creative and innovative web projects, or building design & development tooling.</p>
