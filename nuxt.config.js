fa47e75a306c89df97ca08144dce25fc11d02222
Henry Desroches <henry.desroches@promontech.com>
2019-07-22 09:59:44 -0600

    🔠  - Typography cleanup for header spacing, header anchors, and code blocks

--- nuxt.config.js
+++ nuxt.config.js
@@ -126,11 +126,16 @@ export default {
           containerClass: "post__toc",
           listType: "ol"
         }
+      ],
+      [
+        "markdown-it-anchor",
+        {
+          permalink: true,
+          permalinkBefore: true,
+          permalinkSymbol: "§",
+          level: [2]
+        }
       ]
-      // [
-      //   "markdown-it-anchor",
-      //   { permalink: true, permalinkBefore: true, level: [1, 2, 3] }
-      // ]
     ],
     highlight: (code, lang) => {
       /* eslint-disable no-undef */
