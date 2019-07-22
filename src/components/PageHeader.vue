1bd5308c978847f3513b1085f6d629b04b356473
Henry Desroches <henry.desroches@promontech.com>
2019-07-22 10:46:06 -0600

    📽  - Move header animation into layout component (so animated var persists from page to page)

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -6,33 +6,6 @@
 <script>
 import { TweenLite } from "gsap";
 export default {
-  props: ["title"],
-  data() {
-    return {
-      deltaY: 0,
-      easedDeltaY: 0
-    };
-  },
-  beforeDestroy() {
-    document.removeEventListener("mousemove", this.handleMouseMove);
-  },
-  mounted() {
-    document.addEventListener("mousemove", this.handleMouseMove);
-    this.$nextTick(() => {
-      TweenLite.ticker.addEventListener("tick", this.updateAnimation);
-    });
-  },
-  methods: {
-    handleMouseMove(e) {
-      this.deltaY =
-        ((e.clientY - window.innerHeight / 2) / window.innerHeight) * 2;
-      TweenLite.to(this, 1, {
-        easedDeltaY: this.deltaY
-      });
-    },
-    updateAnimation() {
-      this.$el.style.setProperty("--translateX", `${this.easedDeltaY * 4}vw`);
-    }
-  }
+  props: ["title"]
 };
 </script>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,10 +5,39 @@
   </div>
 </template>
 <script>
+import { TweenLite } from "gsap";
 import Nav from "~/components/Nav";
 export default {
   components: {
     Nav
+  },
+  data() {
+    return {
+      deltaY: 0,
+      easedDeltaY: 0
+    };
+  },
+  beforeDestroy() {
+    document.removeEventListener("mousemove", this.handleMouseMove);
+  },
+  mounted() {
+    console.log("test");
+    document.addEventListener("mousemove", this.handleMouseMove);
+    this.$nextTick(() => {
+      TweenLite.ticker.addEventListener("tick", this.updateAnimation);
+    });
+  },
+  methods: {
+    handleMouseMove(e) {
+      this.deltaY =
+        ((e.clientY - window.innerHeight / 2) / window.innerHeight) * 2;
+      TweenLite.to(this, 1, {
+        easedDeltaY: this.deltaY
+      });
+    },
+    updateAnimation() {
+      this.$el.style.setProperty("--translateX", `${this.easedDeltaY * 4}vw`);
+    }
   }
 };
 </script>
