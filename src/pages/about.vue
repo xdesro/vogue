e4866c6d7dd03a078da4d4110915f5e29f4b167c
Henry Desroches <yo@henry.codes>
2019-10-27 11:30:54 -0600

    🙋🏼‍♀️ - Refactor About page to use more dynamic content

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,7 +3,7 @@
     <PageHeader title="About" />
     <main class="main about" id="main" role="main">
       <figure class="main__img about__profile-pic">
-        <img :src="profilePic.src" :alt="profilePic.alt" />
+        <img :src="person.avatar.src" :alt="person.avatar.alt" />
       </figure>
       <img class="about__logo" src="~/assets/img/icon-logo.svg" alt role="presentation" />
       <h2 class="about__subtitle">
@@ -11,39 +11,14 @@
         <br />
         <span class="about__subtitle--light">{{person.name}}.</span>
       </h2>
-      <div class="about__content" v-html="$md.render(person.shortBio)">
-        <p>I’m a creative developer currently based in Denver, Colorado. I’m really into CSS, animation, developer experience, and deleting code.</p>
-        <p>I love bending the rules of CSS, making gen erative art, and WebGL. I have a lot of experience with style code at scale, JS-agnostic design systems, and static site generators. In my non-code time, I foster greyhounds, tweet too much, and ride bikes.</p>
-        <p>I’m looking for a new role working on design systems, creative and innovative web projects, or building design & development tooling.</p>
-        <p>
-          <nuxt-link to="/contact">Let’s chat!</nuxt-link>
-        </p>
-      </div>
+      <div class="about__content" v-html="$md.render(person.shortBio)" />
       <div class="about__recognition">
         <h3 class="about__recognition-title">Recognition</h3>
         <ul class="recognition-list">
-          <li class="recognition-list__item">
-            <a
-              class="recognition-list__link"
-              href="https://www.awwwards.com/sites/creative-developer-portfolio"
-            >
-              <strong>Awwwards</strong> Honorable Mention
-            </a>
-          </li>
-          <li class="recognition-list__item">
-            <a
-              class="recognition-list__link"
-              href="https://www.awwwards.com/sites/creative-developer-portfolio"
-            >
-              <strong>Awwwards</strong> Mobile Excellence
-            </a>
-          </li>
-          <li class="recognition-list__item">
-            <a
-              class="recognition-list__link"
-              href="https://www.cssdesignawards.com/sites/creative-developer-portfolio/35579"
-            >
-              <strong>CSS Design Awards</strong> Special Kudos
+          <li class="recognition-list__item" v-for="source in person.recognition">
+            <a :href="source.link" class="recognition-list__link">
+              <strong>{{source.publisher}}</strong>
+              &nbsp;{{source.title}}
             </a>
           </li>
         </ul>
@@ -59,12 +34,20 @@ export default {
   components: { PageHeader },
   computed: {
     person() {
-      return this.$store.state.person.person.fields;
-    },
-    profilePic() {
-      return {
+      const fields = this.$store.state.person.person.fields;
+      const { shortBio, name } = this.$store.state.person.person.fields;
+      const recognition = fields.recognition.map(recognition => {
+        return recognition.fields;
+      });
+      const avatar = {
         src: this.$store.state.person.person.fields.image.fields.file.url,
         alt: this.$store.state.person.person.fields.image.fields.description
+      };
+      return {
+        shortBio,
+        name,
+        recognition,
+        avatar
       };
     }
   },
